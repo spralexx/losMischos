167e52d153eb42c60402d37382f057a295244e46
spralexx <renekrick94@gmail.com>
2018-06-01 15:48:14 +0200

    Fri Jun  1 15:48:14 CEST 2018

--- modules/drinkProcessor.js
+++ modules/drinkProcessor.js
@@ -1,14 +1,25 @@
+module.exports.prepare = prepare;
+
 var HX711 = require("hx711");
 const sensor = new HX711(5, 6);
-
 var scale = 450;
 sensor.tare();
 sensor.setScale(scale);
+var reader = setInterval(updateValue, 2000);
 
-module.exports.prepare = prepare;
+var sensorValue = 0;
+
 
 function prepare(req) {
-    console.log(req)
-    console.log(sensor.getUnits());
-    console.log(sensor.getOffset());
+    console.log(sensorValue);
+}
+
+function updateValue() {
+    var newValue = sensor.getUnits();
+
+    if (sensorValue === 0) {
+        sensorValue = newValue
+    } else {
+        sensorValue=(sensorValue+newValue)/2;
+    }
 }
