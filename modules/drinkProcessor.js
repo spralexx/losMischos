35074ae53f9707802fda0a0ca0d06524c25d1a3f
spralexx <renekrick94@gmail.com>
2018-06-01 15:40:57 +0200

    Fri Jun  1 15:40:57 CEST 2018

--- modules/drinkProcessor.js
+++ modules/drinkProcessor.js
@@ -1,11 +1,13 @@
 var HX711 = require("hx711");
 const sensor = new HX711(5, 6);
 
-console.log("hello world");
+var scale = 450;
+sensor.tare();
+sensor.setScale(scale);
 
 module.exports.prepare = prepare;
 
-function prepare(req, sensor) {
+function prepare(req) {
     console.log(req)
     console.log(sensor.getUnits());
 }
