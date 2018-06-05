aa8cc1aa21690874823faf9e604231e3be90e84c
spralexx <renekrick94@gmail.com>
2018-06-05 19:30:34 +0200

    Tue Jun  5 19:30:34 CEST 2018

--- modules/drinkProcessor.js
+++ modules/drinkProcessor.js
@@ -79,10 +79,13 @@ function setupOutputs(soft, alc) {
     fluids.softs = soft;
     fluids.alcs = alc;
     for (var i in fluids.softs) {
+        console.log("setting up: " + fluids.softs[i].output);
         gpio.setup(fluids.softs[i].output, gpio.DIR_OUT);
 
     }
     for (var i in fluids.alcs) {
+        console.log("setting up: " + fluids.alcs[i].output);
+
         gpio.setup(fluids.alcs[i].output, gpio.DIR_OUT);
 
     }
