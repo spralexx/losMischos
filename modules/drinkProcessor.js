e787a6a5153fce42f82cc6d5a19333f10a3ce543
spralexx <renekrick94@gmail.com>
2018-06-06 02:15:40 +0200

    Wed Jun  6 02:15:40 CEST 2018

--- modules/drinkProcessor.js
+++ modules/drinkProcessor.js
@@ -10,7 +10,7 @@ var scale = 450;
 sensor.tare();
 sensor.setScale(scale);
 //setInterval(updateValue,1500);
-var glasSize = 100; //ml
+var glasSize = 150; //ml
 var fluids = {
     softs: [],
     alcs: []
